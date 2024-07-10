c4a15e2c46e1de79bb899a809ade68dec726d996
kjohnm <kjonathas@live.com>
2024-07-10 19:45:32 -0300

    add NavBar and Animated Background

--- src/pages/Home/sections/Hero/Hero.tsx
+++ src/pages/Home/sections/Hero/Hero.tsx
@@ -1,17 +1,24 @@
-import { Button, Container, Grid, styled, Typography } from "@mui/material"
+import { Box, Container, Grid, styled, Typography } from "@mui/material"
 import PerfilPicture from "../../../../assets/images/fotinha.png"
 import DownloadIcon from '@mui/icons-material/Download';
 import ContactPageIcon from '@mui/icons-material/ContactPage';
+import StyledButton from "../../../../components/StyledButton/StyledButton";
+import { AnimatedBackground } from "../../../../components/AnimatedBackground/AnimatedBackground";
 
 const Hero = () => {
 
-  const StyledHero = styled("div")(()=> ({
-    backgroundColor: "black",
+  const StyledHero = styled("div")(({ theme })=> ({
+    backgroundColor: theme.palette.primary.main,
+    height: "100vh",
+    display: "flex",
+    alignItems: "center",
   }))
 
-  const StyledImg = styled("img")(()=> ({
-    width: "100%",
+  const StyledImg = styled("img")(({ theme })=> ({
+    width: "80%",
     borderRadius: "50%",
+    border: "1px solid",
+    borderColor: theme.palette.primary.contrastText,
   }))
 
   return (
@@ -19,24 +26,31 @@ const Hero = () => {
     <StyledHero>
       <Container maxWidth="lg">
         <Grid container spacing={2}>
-          <Grid item xs={12} md={4}>
-            <StyledImg src = {PerfilPicture} />
+          <Grid item xs={12} md={5}>
+            <Box position="relative">
+              <Box position="absolute" width={"125%"} top={-110} right={0}>
+                <AnimatedBackground />
+              </Box>
+              <Box position="relative" textAlign="center">
+                <StyledImg src = {PerfilPicture} />
+              </Box>
+            </Box>
           </Grid>
-          <Grid item xs={12} md={8}>
-            <Typography color="primary" variant="h1"> Jonathas Santos </Typography>
-            <Typography color="primary" variant="h2"> Computer Engineer </Typography>
-            <Grid container display="flex" justifyContent="center">
-              <Grid item xs={12} md={4} display="flex" justifyContent="center">
-                <Button>
+          <Grid item xs={12} md={7}>
+            <Typography color="primary.contrastText" variant="h1"> Jonathas Santos </Typography>
+            <Typography color="primary.contrastText" variant="h2"> Computer Engineer </Typography>
+            <Grid container display="flex" justifyContent="center" spacing={3} pt={3}>
+              <Grid item xs={12} md={5} display="flex" justifyContent="center">
+                <StyledButton>
                   <DownloadIcon/>
-                  Download Resume
-                </Button>
+                    <Typography>Download Resume</Typography>
+                </StyledButton>
               </Grid>
-              <Grid item xs={12} md={4} display="flex" justifyContent="center">
-                <Button>
+              <Grid item xs={12} md={5} display="flex" justifyContent="center">
+                <StyledButton>
                   <ContactPageIcon/>
-                  Contact me
-                </Button>
+                    <Typography>Contact me</Typography>
+                </StyledButton>
               </Grid>
             </Grid>
           </Grid>
